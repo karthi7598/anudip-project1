449328cd2a572254f8b5f3e4a589c447b93bd947
karthi7598 <148150466+karthi7598@users.noreply.github.com>
2023-12-11 11:39:40 +0530

    Add files via upload

---
+++ workspace-spring-tool-suite-4/roadtrip/src/main/java/com/roadtrip/RoadtripApplication.java
@@ -0,0 +1,14 @@
+package com.roadtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadtripApplication.class, args);
+		System.out.println("roadtrip application started");
+	}
+
+}
